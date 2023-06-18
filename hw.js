17612ab190b8cefa80133201870371d043dd6036
andre3500 <new.workpad2891@gmail.com>
2023-06-18 15:45:09 +0300

    solved some

--- hw.js
+++ hw.js
@@ -29,10 +29,13 @@ else {
     Результат виводити в консоль.
 */
 
-let price = 1400;
+let price = 10;
 
 if(price >= 1000 && price <= 1900){
     console.log('це значення знаходиться в діапазоні ціни')
+}
+else {
+    console.log('Увага! Значення поза доступним діапазоном')
 }
 
 /*
@@ -43,25 +46,27 @@ if(price >= 1000 && price <= 1900){
 */
 
 
-let price = 100;
+let price = 10;
 
-if(price >= 1000 && price <= 1900){
-    console.log('це значення знаходиться в діапазоні ціни')
+if (price < 1000 || price > 1900){
+    console.log('це значення поза діапазоном')
 }
 else {
-    console.log('Увага! Значення поза доступним діапазоном')
+    console.log('значення в діапазоні')
 }
 
 
 
-let price = 100;
+let price = 1080;
 
-if(price != 1000 && price != 1900){
-    console.log('це значення знаходиться в діапазоні ціни')
+if (!(price >= 1000 && price <= 1900)) { 
+    console.log('out of the price range')
 }
 else {
-    console.log('Помилка! Значення поза доступним діапазоном')
+    console.log('in a price range')
 }
+
+
 
 /*
 
@@ -190,27 +195,75 @@ console.log(noVowels);
 */
 
 
-const kilometers = 25;
+const kilometers = 0.001;
 const factor = 1000;
 const meters = kilometers * factor;
-const kil ="кілометр";
+const kil ="кілометрів";
 const met ="метрів"
    
-   if(kilometers == 2 || kilometers == 3 || kilometers == 4){
+   if(kilometers % 10 == 1){
+     let kil = "кілометр";
+     console.log(`${kilometers} ${kil} дорівнює ${meters} ${met}. `) 
+   }
+   
+   else if(kilometers >= 11 && kilometers <= 14) {
+     let kil = "кілометрів";
+     console.log(`${kilometers} ${kil} дорівнює ${meters} ${met}. `)
+   }
+   else if(kilometers % 10 == 2 || kilometers % 10 == 3 || kilometers % 10 == 4 && kilometers % 100 == 12 || kilometers % 100 == 13 || kilometers % 100 == 14){
      let kil = "кілометри";
      console.log(`${kilometers} ${kil} дорівнює ${meters} ${met}. `) 
    }
-   else if(kilometers == 0 || kilometers >= 5) {
-     let kil = "кілометрів";
+   
+        else if(kilometers >= 0.001 && kilometers <= 0.49) {
+     let kil = "кілометра";
      console.log(`${kilometers} ${kil} дорівнює ${meters} ${met}. `)
-        else if(kilometers == 0 || kilometers >= 5) {
-     let kil = "кілометрів";
-     console.log(`${kilometers} ${kil} дорівнює ${meters} ${met}. `)
-     
    } 
+   
+   else if(kilometers === 0.001) {
+     let met = "метр";
+     console.log(`${kilometers} ${kil} дорівнює ${meters} ${met}. `)// чогось це працює окремо, але не працює разом. Без цього задача не можу бути виконана повноцінно
+   } 
+   
    else{
    console.log(`${kilometers} ${kil} дорівнює ${meters} ${met}. `)
    }
-  //${}
+  // не зміг нагуглити рішення як додати правило десятичних цифр до інших значень. Наприклад щоб значення 0.345 однаково працювало з 12.345 і 985.345 
+  // 
     
-    //
+  const meters = 21841;
+  const factor = 1000;
+  const kilometers = meters / factor;
+  const kil ="кілометрів";
+  const met ="метрів"
+     
+     if(kilometers % 10 == 1 || meters % 10 == 1){
+       let met = "метр";
+       console.log(`${meters} ${met} дорівнює ${kilometers} ${kil}. `) // так само не знайшов як це зробити: якщо писати 19673 - результат буде 19.673 к-метрІВ і це ок. А якщо написати 21673 - буде 21.~~ кілометрІВ - що не вірно. 
+     }
+     
+     else if(kilometers >= 11 && kilometers <= 14 || meters >= 11 && meters <= 14) {
+       let kil = "кілометрів";
+       let met = "метрів";
+       console.log(`${meters} ${met} дорівнює ${kilometers} ${kil}. `)
+     }
+     else if(kilometers % 10 == 2 || kilometers % 10 == 3 || kilometers % 10 == 4 && kilometers % 100 == 12 || kilometers % 100 == 13 || kilometers % 100 == 14){
+       let kil = "кілометри";
+       let met = "метри";
+       cconsole.log(`${meters} ${met} дорівнює ${kilometers} ${kil}. `)
+     }
+     
+          else if(kilometers >= 0.001 && kilometers <= 0.49) {
+       let kil = "кілометра";
+       let met = "метра";
+       console.log(`${meters} ${met} дорівнює ${kilometers} ${kil}. `)
+     } 
+     
+     else if(kilometers === 0.001 ) {
+       let met = "метр";
+       console.log(`${meters} ${met} дорівнює ${kilometers} ${kil}. `)
+     } 
+     
+     else{
+        console.log(`${meters} ${met} дорівнює ${kilometers} ${kil}. `)
+     }
