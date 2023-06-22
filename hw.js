1c46f8a45f93951be3898a3ed51cf194db13af0f
andre3500 <new.workpad2891@gmail.com>
2023-06-22 19:41:58 +0300

    changed a sum

--- hw.js
+++ hw.js
@@ -104,7 +104,7 @@ let c = 6346;
 
 if (a > b && a < c || a < b && a > c) {console.log ("a is medium")}
 else if (b > a && b < c || b < a && b > c) {console.log ("b is medium")}
-else {console.log("c is medium") }                                      // це абсолютно не те рішення що ви хотіли, але я вирішив надати хоч щось замість пустого місця. Я нажаль так і не зрозумів поставленої задачі, тому знайшов формулу і імплементував її в код
+else {console.log("c is medium") }                                      
 
 /*
 6.  Задано номер дня тижня.
@@ -200,82 +200,35 @@ console.log(noVowels);
     Підказка(https://ukr-lifehacks.ed-era.com/rozdil-9/zvyazok_chislivnykiv)
 */
 
- 
+//meters 
 
+let metr = 3838;
+let kilometr = metr / 1000;
+let arr = [metr, kilometr];
+let result = "";
 
-const kilometers = 0.001;
-const factor = 1000;
-const meters = kilometers * factor;
-const kil ="кілометрів";
-const met ="метрів"
-   
-   if(kilometers % 10 == 1){
-     let kil = "кілометр";
-     console.log(`${kilometers} ${kil} дорівнює ${meters} ${met}. `) 
-   }
-   
-   else if(kilometers >= 11 && kilometers <= 14) {
-     let kil = "кілометрів";
-     console.log(`${kilometers} ${kil} дорівнює ${meters} ${met}. `)
-   }
-   else if(kilometers % 10 == 2 || kilometers % 10 == 3 || kilometers % 10 == 4 && kilometers % 100 == 12 || kilometers % 100 == 13 || kilometers % 100 == 14){
-     let kil = "кілометри";
-     console.log(`${kilometers} ${kil} дорівнює ${meters} ${met}. `) 
-   }
-   
-        else if(kilometers >= 0.001 && kilometers <= 0.49) {
-     let kil = "кілометра";
-     console.log(`${kilometers} ${kil} дорівнює ${meters} ${met}. `)
-   } 
-   
-   else if(kilometers === 0.001) {
-     let met = "метр";
-     console.log(`${kilometers} ${kil} дорівнює ${meters} ${met}. `)// чогось це працює окремо, але не працює разом. Без цього задача не можу бути виконана повноцінно
-   } 
-   
-   else{
-   console.log(`${kilometers} ${kil} дорівнює ${meters} ${met}. `)
-   }
-  // не зміг нагуглити рішення як додати правило десятичних цифр до інших значень. Наприклад щоб значення 0.345 однаково працювало з 12.345 і 985.345 
-  // 
-    
+for(let i = 0; i < arr.length; i++){
+ let number = arr[i];
+ let ending = "";
+ if (number === Math. round (number)) {
+    if (number % 10 === 0 || (number % 10 >= 5 && number % 10 <= 19)) {
+      ending = "ів"
+      
+    } else if (number % 10 === 1) {
+      ending = ""
+    } else if (number % 10 >= 2 && number % 10 <= 4){
+      ending = "и"
+    }
+}
+else {
+      ending = "а"
+}
 
-  const meters = 21841;
-  const factor = 1000;
-  const kilometers = meters / factor;
-  const kil ="кілометрів";
-  const met ="метрів"
-     
-     if(kilometers % 10 == 1 || meters % 10 == 1){
-       let met = "метр";
-       console.log(`${meters} ${met} дорівнює ${kilometers} ${kil}. `) // так само не знайшов як це зробити: якщо писати 19673 - результат буде 19.673 к-метрІВ і це ок. А якщо написати 21673 - буде 21.~~ кілометрІВ - що не вірно. 
-     }
-     
-     else if(kilometers >= 11 && kilometers <= 14 || meters >= 11 && meters <= 14) {
-       let kil = "кілометрів";
-       let met = "метрів";
-       console.log(`${meters} ${met} дорівнює ${kilometers} ${kil}. `)
-     }
-     else if(kilometers % 10 == 2 || kilometers % 10 == 3 || kilometers % 10 == 4 && kilometers % 100 == 12 || kilometers % 100 == 13 || kilometers % 100 == 14){
-       let kil = "кілометри";
-       let met = "метри";
-       cconsole.log(`${meters} ${met} дорівнює ${kilometers} ${kil}. `)
-     }
-     
-          else if(kilometers >= 0.001 && kilometers <= 0.49) {
-       let kil = "кілометра";
-       let met = "метра";
-       console.log(`${meters} ${met} дорівнює ${kilometers} ${kil}. `)
-     } 
-     
-     else if(kilometers === 0.001 ) {
-       let met = "метр";
-       console.log(`${meters} ${met} дорівнює ${kilometers} ${kil}. `)
-     } 
-     
-     else{
-        console.log(`${meters} ${met} дорівнює ${kilometers} ${kil}. `)
-     }
+if(i === 0){
+   result = (`${metr} метр${ending} = `)
+}
+else{result = `${result} ${kilometr} кілометр${ending}`
+}
+}
 
-
-     // задача реалізована не повністю, бо я нажаль так і не зрозумів як вирішити питання, які мене блокують
+console.log(result)
